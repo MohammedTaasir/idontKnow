5b28db9426ebb087f00ccdd1a21648f47444db07
Taasir <taasir2000@gmail.com>
2024-02-18 23:15:49 +0530

    first commit

--- idontKnow/Source.cpp
+++ idontKnow/Source.cpp
@@ -1,116 +1,145 @@
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
-#include <Windows.h>
-#include <gl/GL.h>
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <CL/cl.h>
 #include <CL/cl_gl.h>
 
-#include <stdio.h>
-#include <stdlib.h>
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
 
-static void error_callback(int error, const char* description) {
-    fputs(description, stderr);
-}
+// OpenCL kernel source code
+const char* kernelSource =
+"__kernel void generateRandomTexture(__write_only image2d_t texture) { \n"
+"    const int2 pos = {get_global_id(0), get_global_id(1)}; \n"
+"    uint4 pixel; \n"
+"    pixel.x = rand(); \n"
+"    pixel.y = rand(); \n"
+"    pixel.z = rand(); \n"
+"    pixel.w = 255; \n"
+"    write_imageui(texture, pos, pixel); \n"
+"}";
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
-
-int main(void) {
-    GLFWwindow* window;
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-    window = glfwCreateWindow(512, 512, "Simple example", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
+int main() {
+    // Initialize GLFW and create window
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL-OpenCL Interoperability", nullptr, nullptr);
     glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
 
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
+    // Initialize GLEW
+    glewExperimental = GL_TRUE;
+    glewInit();
 
-    glEnable(GL_TEXTURE_2D);
+    // Set up OpenGL viewport and projection matrix
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, WIDTH, HEIGHT, 0, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+
+    // Create and compile shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n\0";
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Link shaders
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Create VBO for triangle vertices
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,  // bottom left
+         0.5f, -0.5f, 0.0f,  // bottom right
+         0.0f,  0.5f, 0.0f   // top
+    };
+
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Initialize OpenCL
+    cl_platform_id platform;
+    cl_device_id device;
+    cl_context context;
+    cl_command_queue queue;
+    cl_program program;
+    cl_kernel kernel;
+
+    clGetPlatformIDs(1, &platform, NULL);
+    clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+    context = clCreateContext(NULL, 1, &device, NULL, NULL, NULL);
+    queue = clCreateCommandQueue(context, device, 0, NULL);
+    program = clCreateProgramWithSource(context, 1, (const char**)&kernelSource, NULL, NULL);
+    clBuildProgram(program, 1, &device, NULL, NULL, NULL);
+    kernel = clCreateKernel(program, "generateRandomTexture", NULL);
+
+    // Create OpenGL texture objects
     GLuint texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, WIDTH, HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glBindTexture(GL_TEXTURE_2D, 0);
 
-    // Initialize OpenCL
-    cl_platform_id platform_id = NULL;
-    cl_device_id device_id = NULL;
-    cl_uint ret_num_devices;
-    cl_uint ret_num_platforms;
-    cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-    ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+    // Register OpenGL texture object with OpenCL
+    cl_mem clTexture = clCreateFromGLTexture(context, CL_MEM_WRITE_ONLY, GL_TEXTURE_2D, 0, texture, NULL);
 
-    cl_context_properties properties[] = {
-        CL_CONTEXT_PLATFORM, (cl_context_properties)platform_id,
-        CL_GL_CONTEXT_KHR, (cl_context_properties)wglGetCurrentContext(),
-        CL_WGL_HDC_KHR, (cl_context_properties)wglGetCurrentDC(),
-        0
-    };
+    // Set kernel arguments
+    clSetKernelArg(kernel, 0, sizeof(cl_mem), &clTexture);
 
-    cl_context ctx = clCreateContext(properties, 1, &device_id, NULL, NULL, &ret);
-    if (ret != CL_SUCCESS)
-        printf("Context creation => OpenCL error: %d\n", ret);
+    // Execute OpenCL kernel
+    size_t global_work_size[2] = { WIDTH, HEIGHT };
+    clEnqueueNDRangeKernel(queue, kernel, 2, NULL, global_work_size, NULL, 0, NULL, NULL);
+    clFinish(queue);
 
-    // Create a command queue
-    cl_command_queue queue = clCreateCommandQueue(ctx, device_id, 0, &ret);
+    // Cleanup OpenCL resources
+    clReleaseMemObject(clTexture);
+    clReleaseKernel(kernel);
+    clReleaseProgram(program);
+    clReleaseCommandQueue(queue);
+    clReleaseContext(context);
 
-    //cl_command_queue queue = clCreateCommandQueue(ctx, device_id, CL_QUEUE_PROFILING_ENABLE, &ret);
-
-
-
-    // Create Image
-    cl_mem image = clCreateFromGLTexture2D(ctx, CL_MEM_READ_WRITE, GL_TEXTURE_2D, 0, texture, &ret);
-    if (ret != CL_SUCCESS)
-        printf("Texture creation => OpenCL error: %d\n", ret);
-
-    // Create Buffer RGB * width * height
-    cl_mem buffer = clCreateBuffer(ctx, CL_MEM_WRITE_ONLY, 3 * width * height, NULL, &ret);
-    if (ret != CL_SUCCESS)
-        printf("Buffer creation => OpenCL error: %d\n", ret);
-
+    // Main loop
     while (!glfwWindowShouldClose(window)) {
-        float ratio;
-        ratio = width / (float)height;
-        glViewport(0, 0, width, height);
+        // Clear the screen
         glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef(0.f, 0.f, 0.f, 1.f);
-        glBindTexture(GL_TEXTURE_2D, texture);
-        glBegin(GL_QUADS);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-1.f, -1.f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(-1.f, 1.f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(1.f, 1.f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(1.f, -1.f, 0.f);
-        glEnd();
+
+        // Bind the shaders and VBO
+        glUseProgram(shaderProgram);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        // Render the triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Swap buffers and poll events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    ret = clFlush(queue);
-    ret = clFinish(queue);
-    ret = clReleaseMemObject(image);
-    ret = clReleaseMemObject(buffer);
-    ret = clReleaseCommandQueue(queue);
-    ret = clReleaseContext(ctx);
-
-    glfwDestroyWindow(window);
+    // Cleanup GLFW
     glfwTerminate();
-    exit(EXIT_SUCCESS);
+
+    return 0;
 }
